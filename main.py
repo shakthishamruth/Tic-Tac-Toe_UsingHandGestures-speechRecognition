ac150e429d9f486d4090b723b667a9c561ffb69a
shakthishamruth <shakthishamruth@gmail.com>
2022-12-27 19:39:23 +0530

    Added GestureControl

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pygame
 import random
 import cv2
+import mediapipe as mp
+import statistics
+import time
 
 pygame.init()
+
+# mediapipe
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
 
 # Window
 screen = pygame.display.set_mode((800, 675))
@@ -150,9 +161,9 @@ def check_Draw():
 def bot():
     global xo, board, player1score, winner
     if win == 1:
-        winner = 'Player wins'
+        winner = 'Player Wins'
     elif win == 2:
-        winner = 'Game Over!'
+        winner = 'Player Lost'
     elif win == 3:
         winner = '     DRAW'
     else:
@@ -175,6 +186,20 @@ def mouse_input(x1, y1, a, b):
         elif player == 1:
             check(a, b)
         winner = ''
+
+
+def gesture_input(a, b):
+    global player, board, xo, winner, reset, win
+    if reset:
+        for i in range(3):
+            for j in range(3):
+                board[i][j] = 0
+                xo[i][j] = ''
+        win = 0
+        reset = False
+    elif player == 1:
+        check(a, b)
+    winner = ''
 
 
 def showXO(cordx, cordy, a, b):
@@ -203,35 +228,145 @@ def show_winner():
 
 
 # Main loop
-while running:
-    screen.fill((0, 0, 0))
-    screen.blit(background, (0, 0))
-    # Events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            x, y = pygame.mouse.get_pos()
-            mouse_input(130, 50, 1, 1)
-            mouse_input(310, 50, 1, 2)
-            mouse_input(490, 50, 1, 3)
-            mouse_input(130, 230, 2, 1)
-            mouse_input(310, 230, 2, 2)
-            mouse_input(490, 230, 2, 3)
-            mouse_input(130, 410, 3, 1)
-            mouse_input(310, 410, 3, 2)
-            mouse_input(490, 410, 3, 3)
-            mouse_input_reset(20, 630)
-    showXO(130, 50, 1, 1)
-    showXO(310, 50, 1, 2)
-    showXO(490, 50, 1, 3)
-    showXO(130, 230, 2, 1)
-    showXO(310, 230, 2, 2)
-    showXO(490, 230, 2, 3)
-    showXO(130, 410, 3, 1)
-    showXO(310, 410, 3, 2)
-    showXO(490, 410, 3, 3)
-    bot()
-    show_winner()
-    show_player_turn_score()
-    pygame.display.update()
+with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.8,
+        min_tracking_confidence=0.8) as hands:
+    while running:
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Initially set finger count to 0 for each cap
+        fingerCount = 0
+        countset = []
+
+        for i in range(0, 50):
+            if results.multi_hand_landmarks:
+
+                for hand_landmarks in results.multi_hand_landmarks:
+                    # Get hand index to check label (left or right)
+                    handIndex = results.multi_hand_landmarks.index(hand_landmarks)
+                    handLabel = results.multi_handedness[handIndex].classification[0].label
+
+                    # Set variable to keep landmarks positions (x and y)
+                    handLandmarks = []
+
+                    # Fill list with x and y positions of each landmark
+                    for landmarks in hand_landmarks.landmark:
+                        handLandmarks.append([landmarks.x, landmarks.y])
+
+                    # Test conditions for each finger: Count is increased if finger is
+                    #   considered raised.
+                    # Thumb: TIP x position must be greater or lower than IP x position,
+                    #   depending on hand label.
+                    if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
+                        fingerCount = fingerCount + 1
+                    elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
+                        fingerCount = fingerCount + 1
+
+                    # Other fingers: TIP y position must be lower than PIP y position,
+                    #   as image origin is in the upper left corner.
+                    if handLandmarks[8][1] < handLandmarks[6][1]:  # Index finger
+                        fingerCount = fingerCount + 1
+                    if handLandmarks[12][1] < handLandmarks[10][1]:  # Middle finger
+                        fingerCount = fingerCount + 1
+                    if handLandmarks[16][1] < handLandmarks[14][1]:  # Ring finger
+                        fingerCount = fingerCount + 1
+                    if handLandmarks[20][1] < handLandmarks[18][1]:  # Pinky
+                        fingerCount = fingerCount + 1
+
+                    # Draw hand landmarks
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+            countset.append(fingerCount)
+            time.sleep(0.0001)
+
+        fingerCount = statistics.mode(countset)
+
+        # Display finger count
+        cv2.putText(image, str(fingerCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
+
+        # Display image
+        cv2.imshow('Camera', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+        # Events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = pygame.mouse.get_pos()
+                mouse_input(130, 50, 1, 1)
+                mouse_input(310, 50, 1, 2)
+                mouse_input(490, 50, 1, 3)
+                mouse_input(130, 230, 2, 1)
+                mouse_input(310, 230, 2, 2)
+                mouse_input(490, 230, 2, 3)
+                mouse_input(130, 410, 3, 1)
+                mouse_input(310, 410, 3, 2)
+                mouse_input(490, 410, 3, 3)
+                mouse_input_reset(20, 630)
+
+        match fingerCount:
+            case 1:
+                gesture_input(1, 1)
+                continue
+            case 2:
+                gesture_input(1, 2)
+                continue
+            case 3:
+                gesture_input(1, 3)
+                continue
+            case 4:
+                gesture_input(2, 1)
+                continue
+            case 5:
+                gesture_input(2, 2)
+                continue
+            case 6:
+                gesture_input(2, 3)
+                continue
+            case 7:
+                gesture_input(3, 1)
+                continue
+            case 8:
+                gesture_input(3, 2)
+                continue
+            case 9:
+                gesture_input(3, 3)
+                continue
+        showXO(130, 50, 1, 1)
+        showXO(310, 50, 1, 2)
+        showXO(490, 50, 1, 3)
+        showXO(130, 230, 2, 1)
+        showXO(310, 230, 2, 2)
+        showXO(490, 230, 2, 3)
+        showXO(130, 410, 3, 1)
+        showXO(310, 410, 3, 2)
+        showXO(490, 410, 3, 3)
+        bot()
+        show_winner()
+        show_player_turn_score()
+        pygame.display.update()
+cap.release()
